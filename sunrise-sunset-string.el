0724a1d9543d862cee8acdb48c661a565d0be63a
letoh <letoh.tw@gmail.com>
2013-05-11 13:43:33 +0800

    import

---
+++ sunrise-sunset-string.el
@@ -0,0 +1,19 @@
+
+(defun get-sunrise-sunset-string ()
+  "get the real result from `sunrise-sunset'"
+  (save-window-excursion
+    (let ((regex  "[0-9]+:[0-9]+[ap]m")
+	  (s      (sunrise-sunset))
+	  (buf    (get-buffer "*temp*")))
+      (unless (and (stringp s)
+		   (string-match-p regex s)
+		   (not buf))
+	(with-current-buffer buf
+	  (let* ((s1  (buffer-string))
+		 (s2  (if (string-match-p regex s1)
+			  s1 nil)))
+	    (setq s s2)
+	    (kill-buffer buf))))
+      s)))
+
+(get-sunrise-sunset-string)
