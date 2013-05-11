c2e85b7503e77aa7750a7c4ff8761cc4351b61e3
letoh <letoh.tw@gmail.com>
2013-05-11 14:18:14 +0800

    fix buffer checking

--- sunrise-sunset-string.el
+++ sunrise-sunset-string.el
@@ -7,13 +7,13 @@
 	  (s      (sunrise-sunset))
 	  (buf    (get-buffer "*temp*")))
       (unless (and (stringp s)
-		   (string-match-p regex s)
-		   (not buf))
-	(with-current-buffer buf
-	  (let* ((s1  (buffer-string))
-		 (s2  (if (string-match-p regex s1)
-			  s1 nil)))
-	    (setq s s2)
-	    (kill-buffer buf))))
+		   (string-match-p regex s))
+	(when buf
+	  (with-current-buffer buf
+	    (let* ((s1  (buffer-string))
+		   (s2  (if (string-match-p regex s1)
+			    s1 nil)))
+	      (setq s s2)
+	      (kill-buffer buf)))))
       s)))
 
